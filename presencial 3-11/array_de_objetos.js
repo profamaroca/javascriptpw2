eb378208e07a7a185b1186f622461ff23d8825ec
profamaroca <profamaroca@gmail.com>
2021-11-03 22:21:23 -0300

    pesquisando com valores da pessoa usuária

--- presencial 3-11/array_de_objetos.js
+++ presencial 3-11/array_de_objetos.js
@@ -14,13 +14,13 @@ let alune2 = {
 let alune3 = {
     nome: "Vitoria",
     curso: "DS",
-    modulo: 2
+    modulo: 1
     }
 
 let alune4 = {
     nome: "Jennifer",
     curso: "DS",
-    modulo: 2
+    modulo: 3
     }
 
 
@@ -43,8 +43,28 @@ let alunes = [alune1, alune2, alune3, alune4]
 console.log('utilizando forEach: ')
 alunes.forEach(mostraAlune);
 
-// prática:
-// montar um array de objetos pessoas como os da lousa
-// usar o forEach para mostrar apenas pessoas com 18 anos ou mais
 
+
+
+// prática:
+// a) montar um array de objetos pessoas como os da lousa
+// b) usar o forEach para exibir todas as pessoas do array
+// c) usar o forEach para mostrar apenas pessoas com 18 anos ou mais
+// d) usar o forEach para mostrar apenas pessoas com uma idade mínima definida pelo usuário
+
+
+console.log('busca pela minima');
+
+// instalar com o comando npm install readline-sync
+// puxa o modulo para dentro deste arquivo
+const readline = require('readline-sync')
+
+// solicita uma informação
+const moduloBusca = readline.question('qual o modulo buscado? ')
+
+function mostraModulo(alune) {
+    if (alune.modulo == moduloBusca) console.log(alune.nome + ' está no módulo ' + moduloBusca);
+}
+
+alunes.forEach(mostraModulo);
 
